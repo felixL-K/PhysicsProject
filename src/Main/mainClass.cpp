db78e5889511776b1419d2148daf6d6de266d694
kahnf <felix.loyau-kahn@etu.u-paris.fr>
2023-09-13 20:36:36 +0200

    chqnge velocity to maximum at clossest to sun

--- src/Main/mainClass.cpp
+++ src/Main/mainClass.cpp
@@ -15,21 +15,21 @@ int main() {
     // for(int i=0;i<100;i++)
     //     system->generateRandomPlanet();
 
-    Planet mercure{3.3*pow(10,23),Vector2D{0,GlobalValues::ASTROUNIT*0.39},system};
+    Planet mercure{3.33010*pow(10,23),Vector2D{0,46000000000},system};
     mercure.setDiameter(4900000);
-    mercure.setVelocity(Vector2D{47900,0});
+    mercure.setVelocity(Vector2D{58970,0});
 
-    Planet venus{4.9*pow(10,24),Vector2D{0,GlobalValues::ASTROUNIT*0.72},system};
+    Planet venus{4.8673*pow(10,24),Vector2D{0,107480000000},system};
     venus.setDiameter(12100000);
-    venus.setVelocity(Vector2D{35000,0});
+    venus.setVelocity(Vector2D{35260,0});
 
-    Planet earth{6*pow(10,24),Vector2D{0,GlobalValues::ASTROUNIT},system};
+    Planet earth{5.9722*pow(10,24),Vector2D{0,147095000000},system};
     earth.setDiameter(12700000);
-    earth.setVelocity(Vector2D{29800,0});
+    earth.setVelocity(Vector2D{30290,0});
 
-    Planet mars{6.4*pow(10,23),Vector2D{0,GlobalValues::ASTROUNIT*1.52},system};
+    Planet mars{6.4169*pow(10,23),Vector2D{0,206650000000},system};
     mars.setDiameter(6800000);
-    mars.setVelocity(Vector2D{24100,0});
+    mars.setVelocity(Vector2D{26500,0});
 
     guiMain g{};
     g.play(system);
