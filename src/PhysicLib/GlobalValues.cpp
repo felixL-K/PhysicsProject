7aba962144af20b5858a997f486916562636edd3
fnifnou <fnifnou@gmail.com>
2023-09-13 16:53:44 +0200

    test token

--- src/PhysicLib/GlobalValues.cpp
+++ src/PhysicLib/GlobalValues.cpp
@@ -8,6 +8,7 @@ double GlobalValues::SOLARDIAMETER = 1400000000; // in m
 double GlobalValues::ASTROUNIT = 150000000000; // in m
 double GlobalValues::GRAVITATIONALCONSTANT = 6.674*pow(10,-11);
 
+
 // double GlobalValues::getDeltaTime() {
 //     return 1/(double)PHYSICS_TICK_SPEED*timeScale;
 //     // return 86400; // number of seconds in a day
